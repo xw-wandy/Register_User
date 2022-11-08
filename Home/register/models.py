21a2255488df3e50f027b80041aa0a21b1142eef
XW <xw.wandy@outlook.com>
2022-11-08 14:50:02 -0400

    Conflicto reparado

--- Home/register/models.py
+++ Home/register/models.py
@@ -9,6 +9,3 @@ class RegisterUser(models.Model):
       name_user = models.CharField(max_length=100)
       email_user = models.CharField(max_length=100)
       id_user = models.CharField(max_length=100)
-
-      def __str__(self) -> str:
-                  return self.name_user 
